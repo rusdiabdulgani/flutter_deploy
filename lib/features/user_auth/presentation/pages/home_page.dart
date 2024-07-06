68be9f274d1691d33d23985afebd0774e9e42655
rusdiabdulgani <rusdi.abdul.gani@unida.ac.id>
2024-07-07 00:20:14 +0700

    tugas mobile programming deploy apk flutter

---
+++ lib/features/user_auth/presentation/pages/home_page.dart
@@ -0,0 +1,320 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _addressController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text("HomePage"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Navigator.pushNamed(context, "/login");
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                _showCreateDataForm(context);
+              },
+              child: Container(
+                height: 45,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    "Create Data",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: _buildUserList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserList() {
+    return Consumer<UserProvider>(
+      builder: (context, userProvider, child) {
+        return DataTable(
+          columns: [
+            DataColumn(label: Text('Nama')),
+            DataColumn(label: Text('Alamat')),
+            DataColumn(label: Text('Umur')),
+            DataColumn(label: Text('Aksi')),
+          ],
+          rows: userProvider.users.map((user) {
+            return DataRow(
+              cells: [
+                DataCell(Text(user.username ?? '')),
+                DataCell(Text(user.adress ?? '')),
+                DataCell(Text(user.age?.toString() ?? '')),
+                DataCell(
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          _showEditDataForm(context, user);
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          userProvider.removeUser(user);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+
+  void _showCreateDataForm(BuildContext context) {
+    _usernameController.clear();
+    _addressController.clear();
+    _ageController.clear();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Create Data'),
+          content: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(labelText: 'Nama'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Masukkan Nama';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _addressController,
+                  decoration: InputDecoration(labelText: 'Alamat'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Masukkan Alamat';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _ageController,
+                  decoration: InputDecoration(labelText: 'Umur'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Masukkan Umur';
+                    }
+                    if (int.tryParse(value) == null) {
+                      return 'Masukkan Umur yang Valid';
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  Provider.of<UserProvider>(context, listen: false).addUser(
+                    UserModel(
+                      username: _usernameController.text,
+                      adress: _addressController.text,
+                      age: int.parse(_ageController.text),
+                    ),
+                  );
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Create'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showEditDataForm(BuildContext context, UserModel user) {
+    _usernameController.text = user.username ?? '';
+    _addressController.text = user.adress ?? '';
+    _ageController.text = user.age?.toString() ?? '';
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Edit Data'),
+          content: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(labelText: 'Nama'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Masukkan Nama';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _addressController,
+                  decoration: InputDecoration(labelText: 'Alamat'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Masukkan Alamat';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _ageController,
+                  decoration: InputDecoration(labelText: 'Umur'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Masukkan Umur';
+                    }
+                    if (int.tryParse(value) == null) {
+                      return 'Masukkan Umur yang Valid';
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  Provider.of<UserProvider>(context, listen: false).updateUser(
+                    user.id!,
+                    UserModel(
+                      id: user.id,
+                      username: _usernameController.text,
+                      adress: _addressController.text,
+                      age: int.parse(_ageController.text),
+                    ),
+                  );
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class UserModel {
+  final String? id;
+  final String? username;
+  final String? adress;
+  final int? age;
+
+  UserModel({this.id, this.username, this.adress, this.age});
+}
+
+class UserProvider with ChangeNotifier {
+  List<UserModel> _users = [];
+  int _idCounter = 0;
+
+  List<UserModel> get users => _users;
+
+  void addUser(UserModel user) {
+    user = UserModel(
+      id: (_idCounter++).toString(),
+      username: user.username,
+      adress: user.adress,
+      age: user.age,
+    );
+    _users.add(user);
+    notifyListeners();
+  }
+
+  void updateUser(String id, UserModel updatedUser) {
+    final index = _users.indexWhere((user) => user.id == id);
+    if (index != -1) {
+      _users[index] = updatedUser;
+      notifyListeners();
+    }
+  }
+
+  void removeUser(UserModel user) {
+    _users.removeWhere((u) => u.id == user.id);
+    notifyListeners();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_firebase/features/app/splash_screen/splash_screen.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/home_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/login_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/sign_up_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyCsHDQtI9DItQgSqwy45_y2xG9tDGxuER8",
+        appId: "1:540215271818:web:8b22d4aee01acdce862873",
+        messagingSenderId: "540215271818",
+        projectId: "flutter-firebase-9c136",
+        // Your web Firebase config options
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter by Rusdi',
+        routes: {
+          '/': (context) => SplashScreen(
+            child: LoginPage(),
+          ),
+          '/login': (context) => LoginPage(),
+          '/signUp': (context) => SignUpPage(),
+          '/home': (context) => HomePage(),
+        },
+      ),
+    );
+  }
+}
